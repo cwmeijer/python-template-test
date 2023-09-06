17cd34dff7022ca410ec2bee584e630d6c36a0cd
Christiaan Meijer <c.meijer@esciencecenter.nl>
2023-09-06 14:46:33 +0200

    First commit

---
+++ my_python_package/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about my_python_package"""
+import logging
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Jane Smith"
+__email__ = "yourname@esciencecenter.nl"
+__version__ = "0.1.0"
